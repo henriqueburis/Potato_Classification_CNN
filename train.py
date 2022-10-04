a088f54a7e236cf731c4ef232d19520e25a9f0fb
Luiz Henrique Buris <henrique.buris@outlook.com.br>
2022-10-03 23:47:30 -0300

    Add files via upload
    commit code

---
+++ train.py
@@ -0,0 +1,432 @@
+#!/usr/bin/env python3 -u
+# Copyright (c) 2017-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the LICENSE file in
+# the root directory of this source tree.
+from __future__ import print_function
+
+import argparse
+import csv
+import os
+
+import numpy as np
+import torch
+from torch.autograd import Variable
+import torch.backends.cudnn as cudnn
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+
+import torchvision.models as models
+from utils import progress_bar
+from torchvision.utils import save_image
+from tensorboardX import SummaryWriter
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+
+parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
+parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
+parser.add_argument('--resume', '-r', action='store_true',
+                    help='resume from checkpoint')
+parser.add_argument('--model', default="ResNet18", type=str,
+                    help='model type (default: ResNet50)')
+parser.add_argument('--name', default='0', type=str, help='name of run')
+parser.add_argument('--seed', default=0, type=int, help='random seed')
+parser.add_argument('--batch_size', default=128, type=int, help='batch size')
+parser.add_argument('--epoch', default=200, type=int,
+                    help='total epochs to run')
+parser.add_argument('--no-augment', dest='augment', action='store_false',
+                    help='use standard augmentation (default: True)')
+parser.add_argument('--decay', default=1e-4, type=float, help='weight decay')
+parser.add_argument('--alpha', default=1., type=float,
+                    help='mixup interpolation coefficient (default: 1)')
+args = parser.parse_args()
+
+use_cuda = torch.cuda.is_available()
+
+writer = SummaryWriter()
+
+best_acc = 0  # best test accuracy
+start_epoch = 0  # start from epoch 0 or last checkpoint epoch
+save_dir = "results"
+
+if args.seed != 0:
+    torch.manual_seed(args.seed)
+
+# Data
+print('==> Preparing data..')
+if args.augment:
+    transform_train = transforms.Compose([
+        transforms.Resize((128,128)),
+        #transforms.RandomCrop(32, padding=4),
+        #transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465),
+                             (0.2023, 0.1994, 0.2010)),
+    ])
+else:
+    transform_train = transforms.Compose([
+        transforms.Resize((128,128)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465),
+                             (0.2023, 0.1994, 0.2010)),
+    ])
+
+
+transform_test = transforms.Compose([
+    transforms.Resize((128,128)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+
+
+trainset = datasets.ImageFolder(os.path.abspath("/content/PotetoDatesets/train"), transform=transform_train)
+trainloader = torch.utils.data.DataLoader(trainset,
+                                          batch_size=args.batch_size,
+                                          shuffle=True, num_workers=3)
+
+
+testset = datasets.ImageFolder(os.path.abspath("/content/PotetoDatesets/test"), transform=transform_test)
+testloader = torch.utils.data.DataLoader(testset, batch_size=args.batch_size,
+                                         shuffle=False, num_workers=3)
+                                         
+
+
+print('==> Building model..')
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+#net = models.resnet50(pretrained=True)
+net = models.resnet50(weights='ResNet50_Weights.DEFAULT')
+modules = list(net.children())[:-1]
+modules.append(nn.Flatten())
+modules.append(nn.Linear(net.fc.in_features,4))
+net = nn.Sequential(*modules)
+net = net.to(device)
+
+if device == 'cuda':
+    net = torch.nn.DataParallel(net)
+    cudnn.benchmark = True
+
+
+"""
+
+# Model
+if args.resume:
+    # Load checkpoint.
+    print('==> Resuming from checkpoint..')
+    assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
+    checkpoint = torch.load('./checkpoint/ckpt.t7' + args.name + '_'
+                            + str(args.seed))
+    net = checkpoint['net']
+    best_acc = checkpoint['acc']
+    start_epoch = checkpoint['epoch'] + 1
+    rng_state = checkpoint['rng_state']
+    torch.set_rng_state(rng_state)
+else:
+    print('==> Building model..')
+    net = models.__dict__[args.model]()
+
+if not os.path.isdir('results'):
+    os.mkdir('results')
+logname = ('results/log_' + net.__class__.__name__ + '_' + args.name + '_'
+           + str(args.seed) + '.csv')
+
+if use_cuda:
+    net.cuda()
+    net = torch.nn.DataParallel(net)
+    print(torch.cuda.device_count())
+    cudnn.benchmark = True
+    print('Using CUDA..')
+"""
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=args.decay)
+
+def save_model(model):
+    torch.save(model.state_dict(), save_dir + "/"+args.seed+"model.pt")
+
+
+def CreateDir(path):
+        try:
+                os.mkdir(path)
+        except OSError as error:
+                print(error)
+
+
+CreateDir(save_dir)
+
+
+def mixup_data(x, y, alpha=1.0, use_cuda=True):
+    '''Returns mixed inputs, pairs of targets, and lambda'''
+    if alpha > 0:
+        lam = np.random.beta(alpha, alpha)
+    else:
+        lam = 1
+
+    batch_size = x.size()[0]
+    if use_cuda:
+        index = torch.randperm(batch_size).cuda()
+    else:
+        index = torch.randperm(batch_size)
+
+    mixed_x = lam * x + (1 - lam) * x[index, :]
+    y_a, y_b = y, y[index]
+    return mixed_x, y_a, y_b, lam
+
+
+def mixup_criterion(criterion, pred, y_a, y_b, lam):
+    return lam * criterion(pred, y_a) + (1 - lam) * criterion(pred, y_b)
+
+
+
+def train(epoch):
+    print('\nEpoch: %d' % epoch)
+    net.train()
+    train_loss = 0
+    reg_loss = 0
+    correct = 0
+    total = 0
+    for batch_idx, (inputs, targets) in enumerate(trainloader):
+        if use_cuda:
+            inputs, targets = inputs.cuda(), targets.cuda()
+
+        inputs_mixup, targets_a, targets_b, lam = mixup_data(inputs, targets,
+                                                       args.alpha, use_cuda)
+        inputs_mixup, targets_a, targets_b = map(Variable, (inputs_mixup,
+                                                      targets_a, targets_b))
+
+        outputs = net(inputs_mixup)
+
+        loss = mixup_criterion(criterion, outputs, targets_a, targets_b, lam)
+        train_loss += loss
+        _, predicted = torch.max(outputs.data, 1)
+        total += targets.size(0)
+        correct += (lam * predicted.eq(targets_a.data).sum().float()
+                    + (1 - lam) * predicted.eq(targets_b.data).sum().float())
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        progress_bar(batch_idx, len(trainloader),
+                     'Loss: %.3f | Reg: %.5f | Acc: %.3f%% (%d/%d)'
+                     % (train_loss/(batch_idx+1), reg_loss/(batch_idx+1),
+                        100.*correct/total, correct, total))
+        writer.add_scalar('Training/ACC',100.*correct/total, (epoch*len(trainloader.dataset)/args.batch_size)+batch_idx)
+        writer.add_scalar('Training/loss',train_loss/(batch_idx+1),(epoch*len(trainloader.dataset)/args.batch_size)+batch_idx)
+    return (train_loss/batch_idx, reg_loss/batch_idx, 100.*correct/total)
+
+
+
+
+def test(epoch):
+    global best_acc
+    net.eval()
+    test_loss = 0
+    correct = 0
+    total = 0
+    for batch_idx, (inputs, targets) in enumerate(testloader):
+        if use_cuda:
+            inputs, targets = inputs.cuda(), targets.cuda()
+        inputs, targets = Variable(inputs, volatile=False), Variable(targets)
+        outputs = net(inputs)
+
+        _, predicted = torch.max(outputs.data, 1)
+        total += targets.size(0)
+        correct += predicted.eq(targets.data).cpu().sum()
+
+        progress_bar(batch_idx, len(testloader),
+                     'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                     % (test_loss/(batch_idx+1), 100.*correct/total,
+                        correct, total))
+    acc = 100.*correct/total
+    writer.add_scalar('Test/ACC',acc, (epoch*len(testloader.dataset)/args.batch_size)+batch_idx)
+    if epoch == start_epoch + args.epoch - 1 or acc > best_acc:
+        checkpoint(acc, epoch)
+    if acc > best_acc:
+        best_acc = acc
+    return (test_loss/batch_idx, 100.*correct/total)
+
+
+def val(epoch):
+    global best_acc
+    net.eval()
+    test_loss = 0
+    correct = 0
+    total = 0
+    pred_ = []
+    label_ = []
+    for batch_idx, (inputs, targets) in enumerate(testloader):
+        if use_cuda:
+            inputs, targets = inputs.cuda(), targets.cuda()
+        inputs, targets = Variable(inputs, volatile=False), Variable(targets)
+        outputs = net(inputs)
+
+        _, predicted = torch.max(outputs.data, 1)
+
+        pred_.append(predicted.data.cpu().numpy())
+        label_.append(targets.data.cpu().numpy())
+
+        total += targets.size(0)
+        correct += predicted.eq(targets.data).cpu().sum()
+
+        progress_bar(batch_idx, len(testloader),
+                     'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                     % (test_loss/(batch_idx+1), 100.*correct/total,
+                        correct, total))
+    acc = 100.*correct/total
+    if epoch == start_epoch + args.epoch - 1 or acc > best_acc:
+        checkpoint(acc, epoch)
+    if acc > best_acc:
+        best_acc = acc
+    return pred_, label_, (test_loss/batch_idx), (100.*correct/total)
+
+def convert_label_(pred_y,true_l,list_label):
+  y_true = []
+  y_pred = []
+  for i in range(len(pred_y)):
+    for j in range(len(pred_y[i])):
+      y_pred.append(list_label[pred_y[i][j].item()])
+      y_true.append(list_label[true_l[i][j].item()])
+  return np.array(y_true),np.array(y_pred)
+
+def plot_confusion_matrix(cm,
+                          target_names,
+                          title='Confusion matrix',
+                          titleG='Confusion matrix',
+                          cmap=None,
+                          normalize=True):
+    """
+    given a sklearn confusion matrix (cm), make a nice plot
+
+    Arguments
+    ---------
+    cm:           confusion matrix from sklearn.metrics.confusion_matrix
+
+    target_names: given classification classes such as [0, 1, 2]
+                  the class names, for example: ['high', 'medium', 'low']
+
+    title:        the text to display at the top of the matrix
+
+    cmap:         the gradient of the values displayed from matplotlib.pyplot.cm
+                  see http://matplotlib.org/examples/color/colormaps_reference.html
+                  plt.get_cmap('jet') or plt.cm.Blues
+
+    normalize:    If False, plot the raw numbers
+                  If True, plot the proportions
+
+    Usage
+    -----
+    plot_confusion_matrix(cm           = cm,                  # confusion matrix created by
+                                                              # sklearn.metrics.confusion_matrix
+                          normalize    = True,                # show proportions
+                          target_names = y_labels_vals,       # list of names of the classes
+                          title        = best_estimator_name) # title of graph
+
+    Citiation
+    ---------
+    http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
+
+    """
+    import matplotlib.pyplot as plt_
+    import numpy as np
+    import itertools
+
+    accuracy = np.trace(cm) / float(np.sum(cm))
+    misclass = 1 - accuracy
+
+    if cmap is None:
+        cmap = plt_.get_cmap('Blues')
+
+    plt_.figure()
+    plt_.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt_.title(titleG)
+    plt_.colorbar()
+
+    if target_names is not None:
+        tick_marks = np.arange(len(target_names))
+        plt_.xticks(tick_marks, target_names, rotation=45)
+        plt_.yticks(tick_marks, target_names)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+
+    thresh = cm.max() / 1.5 if normalize else cm.max() / 2
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        if normalize:
+            plt_.text(j, i, "{:0.4f}".format(cm[i, j]),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+        else:
+            plt_.text(j, i, "{:,}".format(cm[i, j]),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+
+
+    plt_.tight_layout()
+    plt_.ylabel('Verdade do label')
+    plt_.xlabel('Predicted label')
+    plt_.xlabel('Predição do  label\naccuracy={:0.4f}; misclass={:0.4f}'.format(accuracy, misclass))
+    plt_.savefig(title+'-confusion_matrix.png', dpi=120)
+    plt_.close()
+    #plt.show() 
+
+def checkpoint(acc, epoch):
+    # Save checkpoint.
+    print('Saving..')
+    state = {
+        'net': net,
+        'acc': acc,
+        'epoch': epoch,
+        'rng_state': torch.get_rng_state()
+    }
+    if not os.path.isdir('checkpoint'):
+        os.mkdir('checkpoint')
+    torch.save(state, './checkpoint/ckpt.t7' + args.name + '_'
+               + str(args.seed))
+
+
+def adjust_learning_rate(optimizer, epoch):
+    """decrease the learning rate at 100 and 150 epoch"""
+    lr = args.lr
+    if epoch >= 100:
+        lr /= 10
+    if epoch >= 150:
+        lr /= 10
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+"""
+if not os.path.exists(logname):
+    with open(logname, 'w') as logfile:
+        logwriter = csv.writer(logfile, delimiter=',')
+        logwriter.writerow(['epoch', 'train loss', 'reg loss', 'train acc',
+                            'test loss', 'test acc'])
+"""
+for epoch in range(start_epoch, args.epoch):
+    train_loss, reg_loss, train_acc = train(epoch)
+    test_loss, test_acc = test(epoch)
+    adjust_learning_rate(optimizer, epoch)
+    #with open(logname, 'a') as logfile:
+        #logwriter = csv.writer(logfile, delimiter=',')
+        #logwriter.writerow([epoch, train_loss, reg_loss, train_acc, test_loss,
+                            #test_acc])
+
+save_model(net)
+
+pred, label, test_loss, test_acc = val(epoch)
+
+print(test_acc)
+print(testset.classes)
+
+
+y_pred, y_true = convert_label_(pred,label,testset.classes)
+confusion = confusion_matrix(y_true, y_pred, labels = testset.classes)
+
+print(confusion)
+plot_confusion_matrix(cm = np.array(confusion),normalize = False, target_names = testset.classes, title =' Test', titleG = ' Test')
+
+
